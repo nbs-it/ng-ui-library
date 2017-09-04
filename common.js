d96dc4b1cdfb5ec69ecf8322f68d8304f3f1c33a
zvizvi <5633910@gmail.com>
2017-09-04 16:52:37 +0300

    add dialog

--- common.js
+++ common.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import checkbox from './checkbox/checkbox';
 import numberSpinner from './numberSpinner/numberSpinner';
+import dialog from './dialog/dialog.directive';
 import modalBox from './modalBox/modalBox.directive';
 let commonModule = angular.module('app.common', [
   checkbox,
   numberSpinner,
+  dialog,
   modalBox
 ])
 .name;
---
+++ dialog/dialog.link.js
@@ -0,0 +1,43 @@
+export default function ($window, $timeout) {
+  return function (scope, element, attr, ctrl) {
+    var vm = ctrl;
+    vm.isShown = false;
+    vm.show = function () {
+      vm.isShown = true;
+      $timeout(function () {
+        scope.$apply(function () { });
+      });
+      angular.element($window).on('keydown keypress', function (event) {
+        if (event.which === 27) { // Escape key press.
+          vm.hide();
+          event.stopPropagation();
+        }
+      });
+      angular.element($window).on('click', function (event) {
+        if (event.target !== element) { // click outside of the dialog.
+          vm.hide();
+        }
+      });
+      vm.off = scope.$root.$on('hideMenu', function () {
+        vm.hide();
+      });
+    };
+    vm.hide = function () {
+      vm.isShown = false;
+      $timeout(function () {
+        scope.$apply(function () { });
+      });
+      angular.element($window).off('keydown keypress');
+      angular.element($window).off('click');
+      vm.off();
+    };
+    vm.toggle = function () {
+      vm.isShown === true ? vm.hide() : vm.show();
+    };
+    vm.clickOnElement = function () {
+      if (scope.closeByClick !== false) {
+        vm.toggle();
+      }
+    };
+  };
+}
