6d2ec809c7f7f288f81034b17325dee1ef1af141
zvizvi <5633910@gmail.com>
2017-09-05 12:21:46 +0300

    Revert "modal box"
    This reverts commit 5bc18524d855a591db19a68ca3661b5fbfbf179f.

--- modalBox/modalBox.directive.js
+++ modalBox/modalBox.directive.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import template from './modalBox.html';
 import controller from './modalBox.controller';
+import link from './modalBox.link';
 import './modalBox.scss';
 
 let modalBoxModule = angular.module('modalBox', [])
@@ -26,56 +27,7 @@ function modalBoxDirective ($window, $timeout) {
     controller,
     controllerAs: 'vm',
     bindToController: true,
-    link: function (scope, element, attr, ctrl) {
-      var vm = ctrl;
-
-      vm.close = function () {
-        vm.hide();
-      };
-
-      if (vm.open) {
-        vm.isShown = true;
-      } else {
-        vm.isShown = false;
-      }
-      if (!vm.closeOption) {
-        vm.closeOption = true;
-      }
-      vm.show = function () {
-        vm.isShown = true;
-        $timeout(function () {
-          scope.$apply(function () { });
-          angular.element(element[0].getElementsByClassName('modal-box')).on('click', function (event) {
-            if (event.target === event.currentTarget && vm.closeOption !== 'false') { // click outside of the modal.
-              vm.hide();
-            }
-          });
-        });
-        angular.element($window).on('keydown keypress', function (event) {
-          if (event.which === 27 && vm.closeOption !== 'false') { // Escape key press.
-            vm.hide();
-            event.stopPropagation();
-          }
-        });
-      };
-      vm.hide = function () {
-        vm.isShown = false;
-        if (vm.open) {
-          vm.open = false;
-        }
-        $timeout(function () {
-          scope.$apply(function () { });
-        });
-        angular.element($window).off('keydown keypress');
-        angular.element(element[0].getElementsByClassName('modal-box')).off('click');
-      };
-      vm.toggle = function () {
-        vm.isShown === true ? vm.hide() : vm.show();
-      };
-      scope.$on('modalBoxClose', function () {
-        vm.hide();
-      });
-    }
+    link: link($window, $timeout)
   };
 }
 
