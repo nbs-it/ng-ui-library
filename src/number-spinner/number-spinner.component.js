98d3bf459daf9e6d30647ebe28c90562453dc2ef
zvizvi <5633910@gmail.com>
2017-11-12 18:37:17 +0200

    fix number-spinner

--- src/number-spinner/number-spinner.component.js
+++ src/number-spinner/number-spinner.component.js
@@ -10,7 +10,7 @@ let numberSpinnerComponent = {
     currencyIcon: '@',
     compact: '<',
     totalValue: '<',
-    disabled: '='
+    disabled: '=?'
   },
   template,
   controller,
--- src/number-spinner/number-spinner.controller.js
+++ src/number-spinner/number-spinner.controller.js
@@ -2,10 +2,11 @@ import angular from 'angular';
 
 class NumberSpinnerController {
   constructor ($scope, $timeout) {
-    this.input = angular.element(document.getElementById('number-spinner-number-input'));
+    let vm = this;
+    vm.input = angular.element(document.getElementById('number-spinner-number-input'));
     $scope.$watch('vm.disabled', function () {
-      if (this.disabled) {
-        this.value = 1;
+      if (vm.disabled) {
+        vm.value = 1;
       }
     });
   }
@@ -15,16 +16,19 @@ class NumberSpinnerController {
     this.totalValue = this.totalValue || 0;
     this.currencyIcon = this.currencyIcon || '₪';
   }
+
   plus () {
     if (!this.disabled && this.value < 99) {
       this.value++;
     }
   }
+
   minus () {
     if (!this.disabled && this.value > 1) {
       this.value--;
     }
   }
+
   change () {
     if (isNaN(this.value) || this.value === '') {
       this.value = 1;
