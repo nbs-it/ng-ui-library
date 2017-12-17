ae4af615ffa6d3f13fd205cf18345e8ff984e153
zvizvi <5633910@gmail.com>
2017-12-17 21:15:05 +0200

    exp year to 24

--- src/exp-datepicker/exp-datepicker.controller.js
+++ src/exp-datepicker/exp-datepicker.controller.js
@@ -9,7 +9,7 @@ class ExpDatepickerController {
         '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'
       ],
       years: [
-        '17', '18', '19', '20', '21', '22'
+        '17', '18', '19', '20', '21', '22', '23', '24'
       ]
     };
   }
@@ -19,22 +19,26 @@ class ExpDatepickerController {
     this.selectedMonth = this.month;
     this.choose();
   }
+
   selectYear () {
     this.year = event.target.textContent;
     this.selectedYear = this.year;
     this.choose();
   }
+
   close () {
     this.$scope.$broadcast('closeDialog');
     this.selectedMonth = null;
     this.selectedYear = null;
   }
+
   choose () {
+    let vm = this;
     this.$timeout(function () {
-      this.onChoose({});
+      vm.onChoose({});
     });
-    if (this.selectedMonth != null && this.selectedYear != null) {
-      this.close();
+    if (vm.selectedMonth != null && vm.selectedYear != null) {
+      vm.close();
     }
   }
 }
