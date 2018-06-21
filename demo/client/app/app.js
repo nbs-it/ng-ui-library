341ea09dd4c6345388e5d5f22de58cd0962abf09
zvizvi <5633910@gmail.com>
2018-06-22 01:46:33 +0300

    move import common mdules

--- demo/client/app/app.js
+++ demo/client/app/app.js
@@ -3,12 +3,14 @@ import uiRouter from '@uirouter/angularjs';
 import angularAnimate from 'angular-animate';
 import appConfig from './app.config';
 import Components from './components/components';
+import Common from '../../../src/common';
 import AppComponent from './app.component';
 
 angular.module('app', [
   uiRouter,
   angularAnimate,
-  Components
+  Components,
+  Common
 ])
   .config(appConfig)
   .component('app', AppComponent);
--- demo/client/app/components/components.js
+++ demo/client/app/components/components.js
@@ -1,9 +1,7 @@
 import angular from 'angular';
-import Common from '../../../../src/common';
 import Demo from './demo/demo';
 
 let componentModule = angular.module('app.components', [
-  Common,
   Demo
 ])
   .name;
