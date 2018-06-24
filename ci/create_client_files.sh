1e6a8acddbbfbeb59ae0e585fc8d3973f2c4a17a
zvizvi <5633910@gmail.com>
2018-06-24 21:28:22 +0300

    Fix incloude library commn files

--- ci/create_client_files.sh
+++ ci/create_client_files.sh
@@ -1,4 +1,9 @@
+mv ./src ./demo/client
+mv ./package.json ./demo/client
+sed -i "s/..\/..\/..\/src\/common/..\/src\/common/g" ./demo/client/app/app.js
+npm install npm@latest -g
 npm install --prefix ./demo
+npm install --prefix ./demo/client
 npm run build --prefix ./demo
-mv demo/dist public
-mv demo/client/vendors public
+mv ./demo/dist ./public
+mv ./demo/client/vendors ./public
