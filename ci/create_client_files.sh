9cf66aeced21231ceec62ada63f0aa40e17e5d21
zvizvi <5633910@gmail.com>
2018-06-25 03:33:53 +0300

    gitlab ci script documentation

--- ci/create_client_files.sh
+++ ci/create_client_files.sh
@@ -1,9 +1,16 @@
+# move src files into client folder
 mv ./src ./demo/client
 mv ./package.json ./demo/client
 sed -i "s/..\/..\/..\/src\/common/..\/src\/common/g" ./demo/client/app/app.js
+
+# install npm modules
 npm install npm@latest -g
 npm install --prefix ./demo
 npm install --prefix ./demo/client
+
+# build dist files
 npm run build --prefix ./demo
+
+# move dist files into public folder
 mv ./demo/dist ./public
 mv ./demo/client/vendors ./public
